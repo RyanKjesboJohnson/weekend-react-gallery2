9b6a4406bfa98f555d5758d70f26a355ef8dc48d
Ryan Kjesbo-Johnson <Ryanmark86@gmail.com>
2023-11-25 14:04:11 -0600

    Could not fix first repo so creating new one

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -3,12 +3,44 @@ const router = express.Router();
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
-  // code here
-});
+  const idOfDog = req.params.id;
+  console.log(idOfDog);
+  const sqlText = 
+    `
+    UPDATE gallery
+    SET "likes" = "likes" + 1
+    WHERE id = $1;
+    `
+
+  const sqlValues = [
+    idOfDog
+  ]
+
+  pool.query(sqlText, sqlValues)
+    .then ((results) => {
+      res.sendStatus(200)
+    }).catch ((error) => {
+      res.sendStatus(500)
+    })
+  }
+);
 
 // GET /gallery
 router.get('/', (req, res) => {
-  // code here
+  const sqlText = 
+    `
+    SELECT * FROM gallery
+    ORDER BY "title";
+    `
+
+  pool.query(sqlText)
+  .then ((result) => {
+    console.log(result.rows)
+    res.send(result.rows)
+  }) .catch ((error) => {
+    console.log(error)
+    res.send(500)
+  })
 });
 
 module.exports = router;
